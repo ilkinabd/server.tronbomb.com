8ce5078385e6298550e543e3aed65c20eec05f86
DenyStark <denyskrolevetskyi@gmail.com>
2019-07-26 15:52:50 +0300

    Ref: dice-bets to ws

--- src/workers/dice-bets.js
+++ src/workers/dice-bets.js
@@ -1,16 +1,19 @@
+const { NODE, NODE_TOKEN } = process.env;
+
+const io = require('socket.io-client');
+
 const db = require('@db');
 const { dice } = require('@controllers/node');
 const utils = require('@utils/users');
 
-const lastTimestamp = (events, from) => {
-  let last = from;
+const socket = io.connect(NODE, { reconnect: true });
 
-  for (const bet of events) {
-    const { timestamp } = bet;
-    last = Math.max(last, timestamp);
-  }
-  return last + 1;
-};
+socket.on('connect', () => {
+  socket.emit('subscribe', {
+    room: 'dice',
+    token: NODE_TOKEN,
+  });
+});
 
 const updateUserLevel = async(userId) => {
   const betsSum = await db.bets.getSum({ userId });
@@ -18,38 +21,21 @@ const updateUserLevel = async(userId) => {
   db.users.setLevel({ userId, level });
 };
 
-const processEvents = async(events) => {
-  for (const data of events) {
-    const {
-      gameId: index, player: wallet, finishBlock, amount: bet, number, roll
-    } = data.result;
+const processEvent = async(data) => {
+  const { index, wallet, finishBlock, bet, number, roll } = data;
 
-    let userId = await db.users.getId({ wallet });
-    if (!userId) userId = await db.users.add({ wallet });
+  let userId = await db.users.getId({ wallet });
+  if (!userId) userId = await db.users.add({ wallet });
 
-    const response = await dice.getters.game({ gameId: index });
-    const { result, status } = response.game;
+  const gameId = await db.games.add({ index, finishBlock, status: 'start' });
 
-    const gameId = await db.games.add({ index, finishBlock, result, status });
+  const params = JSON.stringify({ number, roll });
+  await db.bets.add({ gameId, userId, bet, params });
+  updateUserLevel(userId);
 
-    const params = JSON.stringify({ number, roll });
-    await db.bets.add({ gameId, userId, bet, params });
-    updateUserLevel(userId);
-
-    if (status === 'start') {
-      setTimeout(() => { dice.functions.finishGame({ gameId: index }); }, 2000);
-    }
-  }
+  setTimeout(() => {
+    dice.functions.finishGame({ gameId: index });
+  }, 2000);
 };
 
-(async() => {
-  let from = 0;
-
-  setInterval(async() => {
-    const data = await dice.events.takeBets({ from });
-    const { events } = data;
-
-    from = lastTimestamp(events, from);
-    processEvents(events);
-  }, 1000);
-})();
+socket.on('dice', processEvent);
