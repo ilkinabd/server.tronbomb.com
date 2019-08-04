b89023e40df6b687452109d96c8cbd69bbe434bf
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-04 22:24:29 +0300

    Ref: rating worker

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require('@workers/dice/finish')(io);
 require('@workers/wheel/start-finish')(io);
 require('@workers/wheel/events')(io);
 
-require('@workers/rating')(io);
+require('@workers/rating')(io.in('rating'));
 
 server.listen(NODE_PORT, NODE_URL, () => {
   console.info(`${NODE_URL}:${NODE_PORT}`);
