adccdda20fbaa980101120cfc333f6043154c77b
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-04 22:06:02 +0300

    Ref: subscribe to portal module

--- src/controllers/portal.js
+++ src/controllers/portal.js
@@ -1,5 +1,5 @@
 const node = require('@controllers/node');
-
+const getResponse = require('@utils/get-response');
 const { resSuccess, resError } = require('@utils/res-builder');
 
 const getConfigs = async(_req, res) => {
@@ -11,6 +11,18 @@ const getConfigs = async(_req, res) => {
   res.json(resSuccess({ contracts }));
 };
 
+const subscribe = async(req, res) => {
+  const { mail } = req.body;
+  const result = await getResponse(mail);
+  if (!result) return res.status(500).json(resError(73500));
+
+  const { status } = result;
+  if (status !== 202) return res.status(500).json(resError(73500));
+
+  res.json(resSuccess());
+};
+
 module.exports = {
   getConfigs,
+  subscribe,
 };
