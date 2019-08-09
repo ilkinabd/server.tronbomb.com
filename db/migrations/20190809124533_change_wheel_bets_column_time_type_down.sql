2a680244ee512c8ca03533607f17c6de5c61942d
Alexandr.Makarov <makar.is.code.jedi@gmail.com>
2019-08-09 17:53:13 +0300

    hot fix: change dice bets time to timestamp

--- db/migrations/20190809124533_change_wheel_bets_column_time_type_down.sql
+++ db/migrations/20190809124533_change_wheel_bets_column_time_type_down.sql
@@ -1,2 +1,6 @@
 ALTER TABLE wheel_bets
 ALTER COLUMN "time" type time without time zone;
+
+ALTER TABLE dice_bets
+ALTER COLUMN "time" type time without time zone;
+
--- db/migrations/20190809124533_change_wheel_bets_column_time_type_up.sql
+++ db/migrations/20190809124533_change_wheel_bets_column_time_type_up.sql
@@ -1,3 +1,7 @@
 ALTER TABLE wheel_bets
 ALTER COLUMN "time" type timestamp without time zone
-USING current_date + "time";
+USING current_date - interval '1 day' + "time";
+
+ALTER TABLE dice_bets
+ALTER COLUMN "time" type timestamp without time zone
+USING current_date - interval '1 day' + "time";
