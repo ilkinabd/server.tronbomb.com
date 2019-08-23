6abecf7b64e948da3983d7c0a9e602e4d218bd7f
Alexandr.Makarov <makar.is.code.jedi@gmail.com>
2019-08-23 17:57:56 +0300

    ref: add enum for status and prize column

--- db/migrations/sqls/20190822154502-create-table-auction-up.sql
+++ db/migrations/sqls/20190822154502-create-table-auction-up.sql
@@ -1,9 +1,15 @@
+CREATE TYPE AUCTION_STATUS AS ENUM(
+    'start',
+    'finish'
+);
+
 CREATE TABLE "auction" (
-  "auction_id" SERIAL      NOT NULL,
-  "user_id"    INTEGER     NOT NULL  REFERENCES "users"("user_id"),
-  "status"     VARCHAR(25),
-  "bet"        FLOAT       NOT NULL,
+  "auction_id" SERIAL         NOT NULL,
+  "user_id"    INTEGER        NOT NULL  REFERENCES "users"("user_id"),
+  "status"     AUCTION_STATUS NOT NULL DEFAULT 'start',
+  "bet"        FLOAT          NOT NULL,
+  "prize"      FLOAT,
   "time"       TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
 
   PRIMARY KEY("auction_id")
-)
+);
