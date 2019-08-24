f4f3e14f492fe24bebd5f291839716a346effaef
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-24 22:34:31 +0300

    Feat: Withdraw referral profit function

--- src/workers/operations.js
+++ src/workers/operations.js
@@ -1,8 +1,11 @@
-const { NODE, NODE_TOKEN, WITHDRAW_FEE } = process.env;
+const {
+  NODE, NODE_TOKEN, WITHDRAW_FEE, MIN_WITHDRAW, MAX_WITHDRAW
+} = process.env;
 
 const io = require('socket.io-client');
 
 const db = require('@db');
+const node = require('@controllers/node');
 
 const socket = io.connect(NODE, { reconnect: true });
 const fee = parseFloat(WITHDRAW_FEE);
@@ -14,10 +17,28 @@ socket.on('connect', () => {
   });
 });
 
+const withdrawReferralProfit = async(wallet, to, amount, txId) => {
+  if (amount < MIN_WITHDRAW || MAX_WITHDRAW < amount) return;
+
+  const profit = await db.users.getRefProfit({ wallet });
+  const delta = amount + fee;
+
+  if (profit < delta) return;
+
+  const type = 'dividends';
+  const payload = await node.tools.withdraw({ to, amount, type });
+  if (!payload || !payload.txID) return;
+
+  await db.refWithdraws.setComplete({ txId, hash: payload.txID });
+  await db.users.setRefProfit({ wallet, delta: -delta });
+};
+
 const referralProfit = async(data) => {
   const { wallet, to, amount } = data;
-  const id = await db.refWithdraws.add({ wallet, amount, to, fee });
-  if (!id) return;
+  const txId = await db.refWithdraws.add({ wallet, amount, to, fee });
+  if (!txId) return;
+
+  withdrawReferralProfit(wallet, to, amount, txId);
 };
 
 socket.on('withdraw-referral-profit', referralProfit);
