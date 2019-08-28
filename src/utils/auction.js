559275f5c113974617aaf16819d8d6c482209efd
Alexandr.Makarov <makar.is.code.jedi@gmail.com>
2019-08-28 18:47:27 +0300

    fix: calculate auction number calculation

--- src/utils/auction.js
+++ src/utils/auction.js
@@ -1,8 +1,8 @@
-const { AUCTION_START } = process.env;
+const { START_AUCTION } = JSON.parse(process.env.AUCTION);
 
 const currentAuctionNumber = () => {
   const day = 24 * 60 * 60 * 1000;
-  const result = Math.round((new Date() - new Date(AUCTION_START)) / day);
+  const result = Math.round((new Date() - new Date(START_AUCTION)) / day);
   return result;
 };
 
