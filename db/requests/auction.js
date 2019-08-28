2bca9c92f8d6c37da4a97b8941d6768f46d81474
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-28 23:48:26 +0300

    Ref: Auction API models

--- db/requests/auction.js
+++ db/requests/auction.js
@@ -16,7 +16,11 @@ module.exports = {
       WHERE "auction_number" = $auctionNumber;`,
 
   'get-by-limit': `
-      SELECT "wallet", "bet", "time"
+      SELECT
+          "wallet",
+          "auction_number" as "auctionNumber",
+          "bet",
+          "time"
       FROM "auction"
       NATURAL JOIN "users"
       WHERE "auction_number" = $auctionNumber
--- src/controllers/auction.js
+++ src/controllers/auction.js
@@ -3,8 +3,8 @@ const { successRes } = require('@utils/res-builder');
 
 const fund = async(_req, res) => {
   const type = 'auction';
-  const { balanceTRX } = await node.fund.balance({ type });
-  successRes(res, { balanceTRX });
+  const { balanceTRX, address } = await node.fund.balance({ type });
+  successRes(res, { balanceTRX, address });
 };
 
 module.exports = {
