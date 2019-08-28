8a0cb4d34054fa0babb1fe2226326f06c6fe4793
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-28 23:57:17 +0300

    Ref: Add round to fund API

--- src/controllers/auction.js
+++ src/controllers/auction.js
@@ -1,10 +1,12 @@
 const node = require('@controllers/node');
+const { currentAuctionNumber } = require('@utils/auction');
 const { successRes } = require('@utils/res-builder');
 
 const fund = async(_req, res) => {
   const type = 'auction';
   const { balanceTRX, address } = await node.fund.balance({ type });
-  successRes(res, { balanceTRX, address });
+  const round = currentAuctionNumber();
+  successRes(res, { balanceTRX, address, round });
 };
 
 module.exports = {
