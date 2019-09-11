a6d35e0be266e4a399f8a7c0f61c072a2f8e0ac1
DenyStark <denyskrolevetskyi@gmail.com>
2019-09-11 21:34:48 +0300

    Feat: randomWinners function

--- src/workers/jackpots/random.js
+++ src/workers/jackpots/random.js
@@ -1,3 +1,27 @@
-module.exports = () => {
-  console.log(new Date(), 'Jackpot');
+const { JACKPOT_MIN_BET_SUM, JACKPOT_WINNERS } = process.env;
+
+const db = require('@db');
+
+const random = (max) => Math.floor(Math.random() * max);
+
+const randomWinners = (members) => {
+  const winners = [];
+
+  for (let i = 0; i < JACKPOT_WINNERS; i++) {
+    const randomIndex = i + random(members.length - i);
+    winners.push(members[randomIndex]);
+    [members[i], members[randomIndex]] = [members[randomIndex], members[i]];
+  }
+
+  return winners;
+};
+
+module.exports = async() => {
+  const users = await db.users.getTop({ limit: 1000 });
+  const members = users.filter(e => e.betSum >= JACKPOT_MIN_BET_SUM)
+    .map(e => e.wallet);
+
+  const winners = randomWinners(members);
+
+  console.log(winners);
 };
