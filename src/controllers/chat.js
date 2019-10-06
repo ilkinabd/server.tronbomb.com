9d49b1f92c26347c22fbaca75e42a3153148566b
xterminate <ilkin5@mail.ru>
2019-10-06 19:40:03 +0300

    Adds controller mthod for telegram auth

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -1,11 +1,20 @@
 const { ADMINS } = JSON.parse(process.env.CHAT);
 
-const db = require('@db');
-const { successRes, errorRes } = require('@utils/res-builder');
+const db = require("@db");
+const { successRes, errorRes } = require("@utils/res-builder");
+const { getUserFromToken } = require("@utils/users");
+const user = async (req, res) => successRes(res, req.user);
 
-const user = async(req, res) => successRes(res, req.user);
+const tuser = async (req, res) => {
+  let token = req.body["access_token"];
+  let user = getUserFromToken(token);
+  await db.oauthUsers.add(user);
+  user.admin = ADMINS.includes(user.index);
+  req.user = user;
+  successRes(res, req.user);
+};
 
-const send = async(req, res) => {
+const send = async (req, res) => {
   const { data } = req.body;
   const { index, name } = req.user;
 
@@ -15,12 +24,29 @@ const send = async(req, res) => {
   const admin = ADMINS.includes(index);
 
   const messages = [{ id, index, name, data, time: new Date(), admin }];
-  process.ws.emit('chat', { messages });
+  process.ws.emit("chat", { messages });
 
   successRes(res);
 };
 
-const setBan = async(req, res) => {
+const tsend = async (req, res) => {
+  let token = req.body["access_token"];
+  req.user = getUserFromToken(token);
+  const { data } = req.body;
+  const { index, name } = req.user;
+
+  const { reason, endTime } = await db.bans.get({ index });
+  if (reason) return errorRes(res, 401, 73402, { reason, endTime });
+  const id = await db.chat.add({ index, data });
+  const admin = ADMINS.includes(index);
+
+  const messages = [{ id, index, name, data, time: new Date(), admin }];
+  process.ws.emit("chat", { messages });
+
+  successRes(res);
+};
+
+const setBan = async (req, res) => {
   const { index, reason, endTime } = req.body;
   if (!req.user.admin) return errorRes(res, 401, 73411);
 
@@ -34,4 +60,6 @@ module.exports = {
   user,
   send,
   setBan,
+  tuser,
+  tsend
 };
