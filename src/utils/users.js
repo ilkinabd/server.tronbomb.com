526284896631f35e01385411d72ae3c8ba877c5b
xterminate <ilkin5@mail.ru>
2019-10-06 19:38:35 +0300

    Gets telegram user from token

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,27 +1,29 @@
 const { REFERRER_PROFIT } = process.env;
 
-const db = require('@db');
-const { toDecimal } = require('@utils/game');
+const db = require("@db");
+const { toDecimal } = require("@utils/game");
+const base64 = require("base-64");
+const utf8 = require("utf8");
 
-const getLevel = (betsSum) => {
+const getLevel = betsSum => {
   const point = 265;
   const coef = 1.12600317;
 
   for (let level = 1; level < 99; level++) {
-    if (betsSum < point * (coef ** (level - 1))) {
+    if (betsSum < point * coef ** (level - 1)) {
       return level;
     }
   }
   return 99;
 };
 
-const updateLevel = async(wallet) => {
+const updateLevel = async wallet => {
   const sum = await db.users.getBetSum({ wallet });
   const level = getLevel(sum);
   db.users.setLevel({ wallet, level });
 };
 
-const referrerProfit = async(wallet, bet) => {
+const referrerProfit = async (wallet, bet) => {
   const referrer = await db.users.getReferrer({ wallet });
   if (!referrer) return;
 
@@ -30,8 +32,21 @@ const referrerProfit = async(wallet, bet) => {
   db.referrals.add({ wallet: referrer, referral: wallet, amount });
 };
 
+const getUserFromToken = token => {
+  let bytes = base64.decode(token);
+  let json = utf8.decode(bytes);
+  let profile = JSON.parse(json);
+  let user =  {
+    index: profile.id,
+    name: `${profile["first_name"]} ${profile["last_name"]}`
+  };
+
+  return user;
+};
+
 module.exports = {
   getLevel,
   updateLevel,
   referrerProfit,
+  getUserFromToken
 };
