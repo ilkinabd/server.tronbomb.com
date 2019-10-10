3f3f6ffd0de6fea9e5492db7108db6bef5fd9dd7
DenyStark <denyskrolevetskyi@gmail.com>
2019-10-10 19:13:03 +0300

    Ref: Eslinter

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -1,20 +1,20 @@
 const { ADMINS } = JSON.parse(process.env.CHAT);
 
-const db = require("@db");
-const { successRes, errorRes } = require("@utils/res-builder");
-const { getUserFromToken } = require("@utils/users");
-const user = async (req, res) => successRes(res, req.user);
+const db = require('@db');
+const { successRes, errorRes } = require('@utils/res-builder');
+const { getUserFromToken } = require('@utils/users');
+const user = async(req, res) => successRes(res, req.user);
 
-const tuser = async (req, res) => {
-  let token = req.body["access_token"];
-  let user = getUserFromToken(token);
+const tuser = async(req, res) => {
+  const token = req.body['access_token'];
+  const user = getUserFromToken(token);
   await db.oauthUsers.add(user);
   user.admin = ADMINS.includes(user.index);
   req.user = user;
   successRes(res, req.user);
 };
 
-const send = async (req, res) => {
+const send = async(req, res) => {
   const { data } = req.body;
   const { index, name } = req.user;
 
@@ -24,13 +24,13 @@ const send = async (req, res) => {
   const admin = ADMINS.includes(index);
 
   const messages = [{ id, index, name, data, time: new Date(), admin }];
-  process.ws.emit("chat", { messages });
+  process.ws.emit('chat', { messages });
 
   successRes(res);
 };
 
-const tsend = async (req, res) => {
-  let token = req.body["access_token"];
+const tsend = async(req, res) => {
+  const token = req.body['access_token'];
   req.user = getUserFromToken(token);
   const { data } = req.body;
   const { index, name } = req.user;
@@ -41,12 +41,12 @@ const tsend = async (req, res) => {
   const admin = ADMINS.includes(index);
 
   const messages = [{ id, index, name, data, time: new Date(), admin }];
-  process.ws.emit("chat", { messages });
+  process.ws.emit('chat', { messages });
 
   successRes(res);
 };
 
-const setBan = async (req, res) => {
+const setBan = async(req, res) => {
   const { index, reason, endTime } = req.body;
   if (!req.user.admin) return errorRes(res, 401, 73411);
 
--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,36 +1,36 @@
-const express = require("express");
+const express = require('express');
 const router = new express.Router();
 
-const controller = require("@controllers/chat");
-const { googleOauth, fbOauth } = require("@middleware/auth");
-const validate = require("@middleware/validate");
+const controller = require('@controllers/chat');
+const { googleOauth, fbOauth } = require('@middleware/auth');
+const validate = require('@middleware/validate');
 
 router
-  .route("/google/user")
-  .post(validate("oauth", false), googleOauth, controller.user);
+  .route('/google/user')
+  .post(validate('oauth', false), googleOauth, controller.user);
 
 router
-  .route("/facebook/user")
-  .post(validate("oauth", false), fbOauth, controller.user);
+  .route('/facebook/user')
+  .post(validate('oauth', false), fbOauth, controller.user);
 
-router.route("/telegram/user").post(validate("oauth", false), controller.tuser);
-
-router
-  .route("/google/send")
-  .post(validate("msg", false), googleOauth, controller.send);
+router.route('/telegram/user').post(validate('oauth', false), controller.tuser);
 
 router
-  .route("/facebook/send")
-  .post(validate("msg", false), fbOauth, controller.send);
-
-router.route("/telegram/send").post(validate("msg", false), controller.tsend);
+  .route('/google/send')
+  .post(validate('msg', false), googleOauth, controller.send);
 
 router
-  .route("/google/set_ban")
-  .post(validate("setBan", false), googleOauth, controller.setBan);
+  .route('/facebook/send')
+  .post(validate('msg', false), fbOauth, controller.send);
+
+router.route('/telegram/send').post(validate('msg', false), controller.tsend);
 
 router
-  .route("/facebook/set_ban")
-  .post(validate("setBan", false), fbOauth, controller.setBan);
+  .route('/google/set_ban')
+  .post(validate('setBan', false), googleOauth, controller.setBan);
+
+router
+  .route('/facebook/set_ban')
+  .post(validate('setBan', false), fbOauth, controller.setBan);
 
 module.exports = router;
