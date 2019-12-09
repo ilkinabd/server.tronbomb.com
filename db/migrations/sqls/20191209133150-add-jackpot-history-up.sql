14367f00935b4fdd60a19621e82ac0f7acd3d887
Alexandr.Makarov <makar.is.code.jedi@gmail.com>
2019-12-09 16:01:59 +0200

    fix: add type to migration

--- db/migrations/sqls/20191209133150-add-jackpot-history-up.sql
+++ db/migrations/sqls/20191209133150-add-jackpot-history-up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "jackpot_history" (
   "jackpot_history_id" INTEGER NOT NULL,
   "user_id"            INTEGER NOT NULL REFERENCES "users"("user_id"),
+  "jackpot_type"       JACKPOT_TYPE NOT NULL,
   "prize"              FLOAT,
   "popup_shown"        BOOLEAN NOT NULL DEFAULT FALSE,
 
