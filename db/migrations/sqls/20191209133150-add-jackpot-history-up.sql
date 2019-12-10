0b152773f3cb6caf7a0dc533141bbd01027815dc
Alexandr.Makarov <makar.is.code.jedi@gmail.com>
2019-12-10 15:22:49 +0200

    delete migration with history

--- db/migrations/sqls/20191209133150-add-jackpot-history-up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "jackpot_history" (
-  "jackpot_history_id" INTEGER NOT NULL,
-  "user_id"            INTEGER NOT NULL REFERENCES "users"("user_id"),
-  "jackpot_type"       JACKPOT_TYPE NOT NULL,
-  "prize"              FLOAT,
-  "popup_shown"        BOOLEAN NOT NULL DEFAULT FALSE,
-
-  PRIMARY KEY("jackpot_history_id")
-)
