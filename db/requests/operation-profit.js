5a62d61ad18ec5501e80979586d8065fbe88afcd
xterminate <ilkin5@mail.ru>
2019-12-20 08:42:31 +0300

    Minus profit to next day

--- db/requests/operation-profit.js
+++ db/requests/operation-profit.js
@@ -1,8 +1,12 @@
 module.exports = {
-  'add': `
+  add: `
       INSERT INTO "operation_profit" ("profit")
       VALUES ($profit)
       RETURNING "profit_id" AS "id";`,
+
+  'get-minus': `
+      SELECT SUM(CASE WHEN COALESCE("profit", 0) < 0 THEN "profit" END ) AS "value"  
+      FROM "operation_profit"`,
 
   'get-no-complete': `
       SELECT COALESCE(SUM("profit"), 0) AS "value"
