73e58cc2f80c0f5d886467f370eb3c578cbbe772
xterminate <ilkin5@mail.ru>
2019-12-26 11:27:47 +0300

    Revert "ge bets by limit" This reverts commit 0457a76aa04cd39dc7788714df0d799258f47cb3.

--- src/controllers/socket.js
+++ src/controllers/socket.js
@@ -24,7 +24,18 @@ const joinBets = async socket => {
   const bets = await db.users.getAllBets({ limit: 25 });
   socket.emit('bets', { bets });
 };
-
+const joinDice = async socket => {
+  const games = await db.dice.getByLimit({ limit: 25 });
+  socket.emit('dice', { games });
+};
+const joinCoin = async socket => {
+  const games = await db.coin.getByLimit({ limit: 25 });
+  socket.emit('coin', { games });
+};
+const joinWheel = async socket => {
+  const bets = await db.wheel.getByLimit({ limit: 25 });
+  socket.emit('wheel', { bets });
+};
 const joinAuction = async socket => {
   const index = getIndex();
   const bets = await db.auction.getByLimit({ index, limit: 100 });
@@ -67,8 +78,17 @@ const joinRoom = (room, socket) => {
     case 'chat':
       joinChat(socket);
       break;
+    case 'dice':
+      joinDice(socket);
+      break;
     case 'bets':
       joinBets(socket);
+      break;
+    case 'coin':
+      joinCoin(socket);
+      break;
+    case 'wheel':
+      joinWheel(socket);
       break;
     case 'auction':
       joinAuction(socket);
