483ef90ff504742b0ca541b7c04bdc22a930c44a
xterminate <ilkin5@mail.ru>
2020-01-09 23:07:38 +0300

    adds wallet to db if not exists

--- src/workers/wallet/events.js
+++ src/workers/wallet/events.js
@@ -3,6 +3,7 @@ const db = require('@db');
 const chargeWallet = async data => {
   console.log('========= Charge wallet ===========');
   const { wallet, amount } = data;
+  db.users.add({ wallet });
   db.users.setBalance({ wallet: wallet, delta: amount });
 };
 
