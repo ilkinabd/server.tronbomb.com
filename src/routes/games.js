2eecec3bfda5a8c90426549118b0d90aeee4846e
xterminate <ilkin5@mail.ru>
2020-01-16 16:15:46 +0300

    fixes games route

--- src/routes/games.js
+++ src/routes/games.js
@@ -3,8 +3,8 @@ const router = new express.Router();
 
 const controller = require('@controllers/games');
 
-router.route('/games/list').post(controller.getList);
+router.route('/list').post(controller.getList);
 
-router.route('/games/sync').post(controller.sync);
+router.route('/sync').post(controller.sync);
 
 module.exports = router;
