8a82d8dc83064cf9b66920b32255233c28ead03c
xterminate <ilkin5@mail.ru>
2020-01-16 16:01:53 +0300

    Adds new games controller

---
+++ src/controllers/games.js
@@ -0,0 +1,72 @@
+const db = require('@db');
+const { PROVIDERS } = JSON.parse(process.env.PROVIDERS);
+const { successRes, errorRes } = require('@utils/res-builder');
+const {
+  IMPERIUM: { URL, HALL, KEY },
+} = PROVIDERS;
+
+const axios = require('axios').create({
+  baseURL: URL,
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+// const getRequest = path => async (params = {}) => {
+//   return await axios.get(URL + path, { params });
+// };
+
+// const postRequest = path => async (params = {}) => {
+//   const res = await axios.post(URL + path, params).catch(console.error);
+//   return res ? res.data : {};
+// };
+
+const getList = async (_req, res) => {
+  games = await db.games.getList({ limit: 10 });
+  successRes(res, { games });
+};
+
+const sync = async (_req, res) => {
+  try {
+    const {
+      data: {
+        content: { gameList },
+      },
+    } = await axios.post('/', {
+      cmd: 'gamesList',
+      hall: HALL,
+      key: KEY,
+    });
+    await db.games.truncate();
+    for (let i = 0; i < gameList.length; i++) {
+      const {
+        id: externalId,
+        name,
+        img,
+        device: isDevice,
+        title: label,
+        flash: isFlash,
+        vertical: isVertical,
+      } = gameList[i];
+
+      db.games
+        .add({
+          name,
+          img,
+          isDevice,
+          label,
+          isFlash,
+          isVertical,
+          externalId,
+        })
+        .catch(console.error);
+    }
+  } catch (error) {
+    errorRes(res, 500, 73500, error);
+  }
+};
+
+module.exports = {
+  getList,
+  sync,
+};
