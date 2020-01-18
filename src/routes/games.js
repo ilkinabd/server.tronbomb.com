24c41699e539e7e29e4eb1a946c60c1d3d30153f
xterminate <ilkin5@mail.ru>
2020-01-18 17:56:39 +0300

    adds multer

--- src/routes/games.js
+++ src/routes/games.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = new express.Router();
+const multer = require('multer');
 
 const controller = require('@controllers/games');
 
-router.route('/api').post(controller.apiCallback);
+router.route('/api').post(multer.none(), controller.apiCallback);
 
 router.route('/list').post(controller.getList);
 
